6d14056e3d889bda3178b57e90eb40d2ecf79e8d
dewerseb <bastien.dewerse@gmail.com>
2020-07-20 10:26:26 +0200

    Structure JS

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,13 +1,47 @@
 score = document.getElementById('score');
+click = document.getElementById('click');
 auto = document.getElementById('auto');
 multiplier = document.getElementById('multiplier');
+bonus = document.getElementById('bonus');
+
+click.addEventListener('click', increaseScore);
+auto.addEventListener('click', buyAuto);
+multiplier.addEventListener('click', buyMultiplier);
+bonus.addEventListener('click', buyBonus);
 
 
-localStorage.setItem('score', '0');
-localStorage.setItem('auto', '0');
-localStorage.setItem('multiplier', '0');
+if (score.innerHTML === "") {
+    localStorage.setItem('score', '0');
+    localStorage.setItem('auto', '0');
+    localStorage.setItem('multiplier', '0');
+}
+
 
 
 function getCost(x) {
     return Math.pow(2, x);
+}
+
+
+
+function increaseScore() {
+
+}
+
+
+
+function buyAuto() {
+
+}
+
+
+
+function buyMultiplier() {
+
+}
+
+
+
+function buyBonus() {
+
 }
